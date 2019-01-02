18f7a1f35bcace546a4b01a5233f57b26f2d6e93
CarysGomezM <43829862+CarysGomezM@users.noreply.github.com>
2019-01-02 16:33:45 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".spell").click(function(){
+ $(".firstImage").slideToggle();
+ });
+ 
+ $(".book").click(function(){
+     $(".secondImage").slideToggle();
+ });
+ 
+ $(".cat").click(function(){
+     $(".cat ").fadeOut();
+ });
+ 
+ 
+
+
